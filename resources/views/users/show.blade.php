f94ef91fe93cf33a54268b7e5f3d3d251571e245
FADev <165898185+francisco-artur-dev@users.noreply.github.com>
2025-08-21 06:22:40 +0100

    Update show.blade.php
    Add funcionalidades novas , responsividade, alteração do código e melhorias

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,28 +1,27 @@
 @extends('layouts.admin')
 
 @section('conteudo da página')
-    <div class="card mt-4 mb-4 border-light shadow">
+<div class="card mt-4 mb-4 border-light shadow">
 
-        <div class="card-header hstack gap-2">
-           <span>Visualizar Usuário</span>
-                <span class="ms-auto d-sm-flex flex-row gap-2">
+    <div class="card-header d-flex flex-column flex-sm-row justify-content-between align-items-start align-items-sm-center gap-2">
+        <span>Visualizar Usuário</span>
+        <span class="d-flex flex-wrap gap-2 mt-2 mt-sm-0">
+            <a href="{{ route('users.index') }}" class="btn btn-info btn-sm">Listar</a>
+            <a href="{{ route('users.edit', ['user' => $user->id]) }}" class="btn btn-warning btn-sm">Editar</a>
+            <form action="{{ route('users.destroy', ['user' => $user->id]) }}" method="POST" style="display:inline;">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger btn-sm"
+                onclick="return confirm('Tem certeza que deseja deletar este usuário?')">Deletar</button>
+            </form>
+        </span>
+    </div>
 
-                    <a href="{{ route('users.index') }}" class="btn btn-info btn-sm">Listar </a>
-                    <a href="{{ route('users.edit', ['user' => $user->id]) }}" class="btn btn-warning btn-sm">Editar </a>
-                    <form action="{{ route('users.destroy', ['user' => $user->id]) }}" method="POST" style="display:inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm"
-                        onclick="return confirm('Tem certeza que deseja deletar este usuário?')">Deletar</button>
-                    </form>
+    <div class="card-body">
+        <x-alert />
 
-                </span>
-       </div>
-
-        <div class="card-body">
-         <x-alert />
-
-            <table class="table">
+        <div class="table-responsive">
+            <table class="table table-striped table-hover">
                 <thead>
                     <tr>
                         <th scope="col">Nome</th>
@@ -42,5 +41,5 @@
             </table>
         </div>
     </div>
+</div>
 @endsection
-
